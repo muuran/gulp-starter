76fb22ee780e260656bc82ff91aca982500bd562
muuran <muramuramuuran@gmail.com>
2015-08-06 23:33:56 +0900

    Delete html templates

--- gulpfile.js/config/html.js
+++ gulpfile.js/config/html.js
@@ -2,9 +2,8 @@ var config = require('./')
 
 module.exports = {
   watch: config.sourceDirectory + '/html/**/*.html',
-  src: [config.sourceDirectory + '/html/**/*.html', '!**/{layouts,shared,macros}/**'],
+  src: [config.sourceDirectory + '/html/**/*.html'],
   dest: config.publicDirectory,
-  nunjucks: [config.sourceDirectory + '/html/'],
   htmlmin: {
     collapseWhitespace: true
   }
